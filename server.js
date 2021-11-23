cb9175926cec1c90e0baf3c1ba97ec5addda1762
Manoj Santra <santramanoj1997@gmail.com>
2021-11-23 09:26:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+const express=require("express")
+const app=express()
+const ejs=require("ejs")
+const expressLayout=require("express-ejs-layouts")
+const path=require("path")
+
+// set Template Engine
+//app.use(expressLayout)
+app.set("views",path.join(__dirname,"/resources/views"))
+app.set("view engine","ejs")
+
+
+// routing
+app.get("/",(req,res)=>{
+  res.render("home")
+})
+
+// server port
+const PORT=process.env.PORT||4000
+
+// server listening
+app.listen(PORT,()=>{
+  console.log(`server is running on port ${PORT}`)
+})
